df404980ddd83f5cd54c8950528d056ee4242ea0
Keith Rieck <KeithRieck@gmail.com>
2024-04-17 20:32:35 -0500

    Added gyro and encoder

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -2,6 +2,8 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.ADIS16470_IMU;
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -18,11 +20,24 @@ public class DriveSubsystem extends SubsystemBase {
     MotorControllerGroup right = new MotorControllerGroup(rightA, rightB);
     
     DifferentialDrive driveController = new DifferentialDrive(left, right);  
+
+    ADIS16470_IMU gyro = new ADIS16470_IMU();
+    Encoder encoder = new Encoder(1, 2);
+
     public DriveSubsystem() {
         left.setInverted(true);
+        encoder.setDistancePerPulse(1.0);
     }
 
     public void arcadeDrive(double speed, double rotation) {
         driveController.arcadeDrive(speed, rotation);
+    }
+
+    public double getGyroAngle() {
+        return gyro.getAngle();
+    }
+
+    public double getDistance() {
+        return encoder.getDistance();
     }
 }
