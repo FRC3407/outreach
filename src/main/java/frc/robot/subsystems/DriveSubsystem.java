3c0dee648b58dd3b85739ee2213827da76de03fb
Isaiah W <ruralanemone@gmail.com>
2024-04-20 11:58:28 -0500

    inverted second left motor

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -9,17 +9,17 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 public class DriveSubsystem extends SubsystemBase {
-    WPI_TalonSRX leftA = new WPI_TalonSRX(0);
+    WPI_TalonSRX left = new WPI_TalonSRX(0);
     WPI_TalonSRX leftB = new WPI_TalonSRX(1);
-    WPI_TalonSRX rightA = new WPI_TalonSRX(2);
+    WPI_TalonSRX right = new WPI_TalonSRX(2);
     WPI_TalonSRX rightB = new WPI_TalonSRX(3);
 
-    MotorControllerGroup left = new MotorControllerGroup(leftA, leftB);
-    MotorControllerGroup right = new MotorControllerGroup(rightA, rightB);
-    
     DifferentialDrive driveController = new DifferentialDrive(left, right);  
     public DriveSubsystem() {
+        leftB.follow(left);
+        rightB.follow(right);
         left.setInverted(true);
+        leftB.setInverted(true);
     }
 
     public void arcadeDrive(double speed, double rotation) {
